952db537b7612f5cbcfb59bfc194aff914d826ab
Sergey Tkachenko <tkachen.s@gmail.com>
2020-01-26 21:46:07 +0300

    Some refactoring

--- BetterCoub.user.js
+++ BetterCoub.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         BetterCoub
-// @namespace    https://coub.com/
+// @namespace    https://github.com/tkachen/better-coub
 // @version      0.1
 // @description  remove promoted stuff and filter content by tags and users
-// @author       You
+// @author       tkachen
 // @match        https://coub.com/*
 // @require      https://raw.githubusercontent.com/uzairfarooq/arrive/master/minified/arrive.min.js
 // @grant        GM_addStyle
@@ -12,42 +12,11 @@
 (function() {
     'use strict';
 
-    var restrictedTags = JSON.parse(localStorage.getItem("blacklist")) || [];
+    var blockedTags = JSON.parse(localStorage.getItem("bc_tag_blacklist")) || [];
+    var blockedUsers = JSON.parse(localStorage.getItem("bc_user_blacklist")) || [];
+    var settings = JSON.parse(localStorage.getItem("bc_settings")) || {};
 
-    function arrayContains(needle, arrhaystack)
-    {
-        return (arrhaystack.indexOf(needle) > -1);
-    }
-
-    function removeByNewTag(tag)
-    {
-        $('.coub__tags-tag[href="/tags/'+tag+'"]').closest('.coub').remove();
-    }
-
-    function addButtonToTagLink(link)
-    {
-        if (!link.hasAttribute('href')) return;
-
-        // remove filtered
-        let tagLink = $(link).attr('href').substring(6);
-        if (arrayContains(tagLink, restrictedTags)) {
-            $(link).closest('.coub').remove();
-        }
-
-        // add filter button
-        $(link).append("<button type='button' class='filterByTag' title='Add tag to Blacklist'>&times;</button>");
-    }
-
-    function addTagToRestricted(tag)
-    {
-        restrictedTags = JSON.parse(localStorage.getItem("blacklist")) || [];
-        if (!arrayContains(tag, restrictedTags)) {
-            restrictedTags.push(tag);
-            localStorage.setItem("blacklist", JSON.stringify(restrictedTags));
-        }
-    }
-
-    $(window).bind("load", function() {
+    function addStyles () {
         GM_addStyle('.related-tags a, .coub__tags-tag { position:relative; }');
 
         // fix for related tags block
@@ -58,17 +27,52 @@
         GM_addStyle('.filterByTag { display: none; position: absolute; right: 0; line-height: inherit; height: inherit; width: 24px; padding: 0; font-weight: bold; font-size: 16px;border: none; background-color: black; color: white; cursor: pointer; vertical-align: middle; border-radius: inherit; }');
         GM_addStyle('.coub__tags-tag:hover .filterByTag, .related-tags a:hover .filterByTag { display: inline-block; }');
         GM_addStyle('.filterByTag:hover { background-color: red; }');
+    }
+
+    function arrayContains (needle, arrhaystack) {
+        return (arrhaystack.indexOf(needle) > -1);
+    }
+
+    function removeByTag (tag) {
+        $('.coub__tags-tag[href="/tags/'+tag+'"]').closest('.coub').remove();
+    }
+
+    // function removeByUser (tag) {
+    //     $('.coub__tags-tag[href="/tags/'+tag+'"]').closest('.coub').remove();
+    // }
+
+    function addButtonToTagLink (link) {
+        if (!link.hasAttribute('href')) return;
+
+        // remove filtered
+        let tagLink = $(link).attr('href').substring(6);
+        if (arrayContains(tagLink, blockedTags)) {
+            $(link).closest('.coub').remove();
+        }
+
+        // add filter button
+        $(link).append("<button type='button' class='filterByTag' title='Add tag to Blacklist'>&times;</button>");
+    }
+
+    function addTagToRestricted (tag) {
+        blockedTags = JSON.parse(localStorage.getItem("blacklist")) || [];
+        if (!arrayContains(tag, blockedTags)) {
+            blockedTags.push(tag);
+            localStorage.setItem("blacklist", JSON.stringify(blockedTags));
+        }
+    }
+
+    $(window).bind("load", function() {
+        addStyles()
 
         $(document).arrive(".coub__promoted-badge", function(el) {
             $(el).closest('.coub').remove();
         });
 
-        //$(".related-tags a").each(function() {
         $("a[href^='/tags/']").each(function() {
             addButtonToTagLink(this);
         });
 
-        //$(document).arrive(".coub__tags-tag:not(.tags-community)", function(newElem) {
         $(document).arrive("a[href^='/tags/']", function(el) {
             addButtonToTagLink(el);
         });
@@ -77,7 +81,7 @@
             e.preventDefault();
             let tag = $(e.target).parent().attr('href').substring(6);
             addTagToRestricted(tag);
-            removeByNewTag(tag);
+            removeByTag(tag);
         });
     });
 })();
